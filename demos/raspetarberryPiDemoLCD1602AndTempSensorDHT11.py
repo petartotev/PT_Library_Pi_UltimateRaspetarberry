9f189793dca76cf2be516b823e55625b2e8bff30
petar@petartotev.net <petar@petartotev.net>
2022-03-06 21:01:21 +0200

    LCD-DHT11 demo runs parallel

--- demos/raspetarberryPiDemoLCD1602AndTempSensorDHT11.py
+++ demos/raspetarberryPiDemoLCD1602AndTempSensorDHT11.py
@@ -2,13 +2,22 @@ import time
 import Adafruit_DHT
 from rpi_lcd import LCD
 from datetime import datetime
-import threading
+import multiprocessing
 
 # pinVcc = 3V3 (1)
 # pinGnd = GND (9)
 pinSignal = 17 # GPIO17 (11)
 
 screen = LCD()
+
+def getDataFromDHT11Once():
+	try:
+		humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pinSignal)
+		return humidity, temperature
+	except RuntimeError as error:
+		print("Runtime error")
+	except Exception as error:
+		print("Exception error")
 
 def getDataFromDHT11():
 	while True:
@@ -28,20 +37,35 @@ def getDataFromDHT11():
 
 def displayTime(isOnLine1):
 	lineToDisplayOn = 1 if isOnLine1 else 2
-	datetimenow = str(datetime.now()).replace('-','')
-	textDate = datetimenow[2:8]
-	textTime = datetimenow[9:17]
-	screen.text(textTime + '  ' + textDate, lineToDisplayOn)
+	try:
+		datetimenow = str(datetime.now()).replace('-','')
+		textDate = datetimenow[2:8]
+		textTime = datetimenow[9:17]
+		screen.text(textTime + '  ' + textDate, lineToDisplayOn)
+		print(textTime + '  ' + textDate)
+	except:
+		errorMessage = "Time failed!"
+		screen.text(errorMessage, lineToDisplayOn)
+		print(errorMessage)
 
 def displayDataFromDHT11(isOnLine1):
 	lineToDisplayOn = 1 if isOnLine1 else 2
-	humidity, temperature = getDataFromDHT11()
-	textTemp = "{:.1f}`C".format(temperature)
-	textHumidity = "{}%".format(humidity)
-	screen.text(textTemp + ', ' + textHumidity, lineToDisplayOn)
+	try:
+		humidity, temperature = getDataFromDHT11Once()
+		textTemp = "{:.1f}`C".format(temperature)
+		textHumidity = "{}%".format(humidity)
+		screen.text(textTemp + ', ' + textHumidity, lineToDisplayOn)
+		print(textTemp + ', ' + textHumidity)
+	except:
+		errorMessage = "DHT11 failed!"
+		screen.text(errorMessage, lineToDisplayOn)
+		print(errorMessage)
 
 while(True):
-	displayTime(True)
-	displayDataFromDHT11(False)
-	#th = threading.Thread(target=displayDataFromDHT11(False))
-	#th.start()
+	#displayTime(True)
+	#displayDataFromDHT11(False)
+	process1 = multiprocessing.Process(target=displayTime, args=(True,))
+	process2 = multiprocessing.Process(target=displayDataFromDHT11, args=(False,))
+	process1.start()
+	process2.start()
+	time.sleep(0.9)
