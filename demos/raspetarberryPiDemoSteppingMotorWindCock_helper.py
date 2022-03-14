785570705afbeab877b480b562cec4807a917fb0
petar@petartotev.net <petar@petartotev.net>
2022-03-14 20:09:36 +0200

    Stepping Motor WindCock demo _01.py takes arguments

---
+++ demos/raspetarberryPiDemoSteppingMotorWindCock_helper.py
@@ -0,0 +1,75 @@
+import time
+import RPi.GPIO as GPIO
+import random
+import requests
+import http.client
+import geocoder
+import json
+import credentials
+import datetime
+import sys
+
+GPIO.setmode(GPIO.BCM)
+
+# STEPPING MOTOR
+
+StepPins = [24,25,8,7]
+
+for pin in StepPins:
+        GPIO.setup(pin,GPIO.OUT)
+        GPIO.output(pin, False)
+
+WaitTime = 0.001
+
+StepCount1 = 4
+Seq1 = []
+Seq1 = [i for i in range(0, StepCount1)]
+Seq1[0] = [1,0,0,0]
+Seq1[1] = [0,1,0,0]
+Seq1[2] = [0,0,1,0]
+Seq1[3] = [0,0,0,1]
+
+StepCount2 = 8
+Seq2 = []
+Seq2 = [i for i in range(0, StepCount2)]
+Seq2[0] = [1,0,0,0]
+Seq2[1] = [1,1,0,0]
+Seq2[2] = [0,1,0,0]
+Seq2[3] = [0,1,1,0]
+Seq2[4] = [0,0,1,0]
+Seq2[5] = [0,0,1,1]
+Seq2[6] = [0,0,0,1]
+Seq2[7] = [1,0,0,1]
+
+Seq = Seq2
+StepCount = StepCount2
+
+def steps(nb):
+        StepCounter = 0
+        if nb<0: sign=-1
+        else: sign=1
+        nb=sign*nb*2 #times 2 because half-step
+        print("nbsteps {} and sign {}".format(nb,sign))
+        for i in range(nb):
+                for pin in range(4):
+                        xpin = StepPins[pin]
+                        if Seq[StepCounter][pin]!=0:
+                                GPIO.output(xpin, True)
+                        else:
+                                GPIO.output(xpin, False)
+                StepCounter += sign
+        # If we reach the end of the sequence
+        # start again
+                if (StepCounter==StepCount):
+                        StepCounter = 0
+                if (StepCounter<0):
+                        StepCounter = StepCount-1
+                # Wait before moving on
+                time.sleep(WaitTime)
+
+degToRotate = sys.argv[1]
+
+try:
+	steps(int(degToRotate))
+except:
+	steps(0)
